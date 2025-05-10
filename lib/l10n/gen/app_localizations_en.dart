aaac911eab8ab6e3e4da362a5a8d22938a2d1949
camtrik <camtrikcsky@gmail.com>
2025-05-10 17:48:15 +0900

    l10n settings

---
+++ lib/l10n/gen/app_localizations_en.dart
@@ -0,0 +1,63 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appTitle => 'Money Manager';
+
+  @override
+  String get categories => 'Categories';
+
+  @override
+  String get transactions => 'Transactions';
+
+  @override
+  String get newTransaction => 'New Transaction';
+
+  @override
+  String get amount => 'Amount';
+
+  @override
+  String get category => 'Category';
+
+  @override
+  String get currency => 'Currency';
+
+  @override
+  String get date => 'Date';
+
+  @override
+  String get save => 'Save';
+
+  @override
+  String get noTransactions => 'No transactions yet';
+
+  @override
+  String get deleted => 'Deleted';
+
+  @override
+  String totalExpense(Object amount) {
+    return 'Total expense: ¥$amount';
+  }
+
+  @override
+  String get settings => 'Settings';
+
+  @override
+  String get language => 'Language';
+
+  @override
+  String get english => 'English';
+
+  @override
+  String get chinese => 'Chinese';
+
+  @override
+  String get japanese => 'Japanese';
+}
---
+++ lib/l10n/gen/app_localizations_ja.dart
@@ -0,0 +1,63 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Japanese (`ja`).
+class AppLocalizationsJa extends AppLocalizations {
+  AppLocalizationsJa([String locale = 'ja']) : super(locale);
+
+  @override
+  String get appTitle => 'マネーマネージャー';
+
+  @override
+  String get categories => 'カテゴリー';
+
+  @override
+  String get transactions => '取引';
+
+  @override
+  String get newTransaction => '新規取引';
+
+  @override
+  String get amount => '金額';
+
+  @override
+  String get category => 'カテゴリー';
+
+  @override
+  String get currency => '通貨';
+
+  @override
+  String get date => '日付';
+
+  @override
+  String get save => '保存';
+
+  @override
+  String get noTransactions => '取引記録がありません';
+
+  @override
+  String get deleted => '取引が削除されました';
+
+  @override
+  String totalExpense(Object amount) {
+    return '今月の支出：¥$amount';
+  }
+
+  @override
+  String get settings => '設定';
+
+  @override
+  String get language => '言語';
+
+  @override
+  String get english => '英語';
+
+  @override
+  String get chinese => '中国語';
+
+  @override
+  String get japanese => '日本語';
+}
